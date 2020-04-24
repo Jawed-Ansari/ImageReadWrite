0bd763b4398823a3c4cb103debdc0e79350a1829
Jawed Ansari <viren@Jaweds-MacBook-Pro.local>
2020-04-24 10:53:37 +0530

    xcode Project add

---
+++ abc/abc/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  abc
+//
+//  Created by Jawed Ansari on 19/04/20.
+//  Copyright © 2020 Jawed Ansari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Jawed")
+        
+    }
+    
+    @IBAction func tapSignUp(_ sender: Any)
+    {
+//      let bundle: Bundle = Bundle.main
+//        let vc: testingViewController = testingViewController(nibName: "testng", bundle: bundle)
+//        self.navigationController?.pushViewController(vc, animated: true)
+        
+        
+    }
+}
+
---
+++ abc/abc/testingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  testingViewController.swift
+//  abc
+//
+//  Created by Jawed Ansari on 22/04/20.
+//  Copyright © 2020 Jawed Ansari. All rights reserved.
+//
+
+import UIKit
+
+class testingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        
+    }
+    
+
+
+}
